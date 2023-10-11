9808abec0c6c02f107d157d166e7872e55b8d971
muzaim <muzaim.surya@yahoo.co.id>
2023-10-11 10:02:23 +0700

    first commit

---
+++ src/components/AnimatedPlaceholder.vue
@@ -0,0 +1,4 @@
+<template>
+  <div class="bg-gradient-to-r from-gray-100 animate-pulse">&nbsp;</div>
+</template>
+
---
+++ src/components/CityList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div v-for="city in savedCities" :key="city.id">
+    <CityCard :city="city" @click="goToCityView(city)" />
+  </div>
+
+  <p v-if="savedCities.length === 0">No cities saved.</p>
+</template>
+
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import CityCard from "../components/CityCard.vue";
+
+const savedCities = ref([]);
+const router = useRouter();
+const getCities = async () => {
+  if (localStorage.getItem("savedCities")) {
+    savedCities.value = JSON.parse(localStorage.getItem("savedCities"));
+
+    const request = [];
+    savedCities.value.forEach((city) => {
+      request.push(
+        axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${city.coords.lat}&lon=${city.coords.lng}&appid=7efa332cf48aeb9d2d391a51027f1a71&units=imperial`
+        )
+      );
+    });
+
+    const weatherData = await Promise.all(request);
+
+    await new Promise((res) => setTimeout(res, 1000));
+
+    weatherData.forEach((value, index) => {
+      savedCities.value[index].weather = value.data;
+    });
+  }
+};
+await getCities();
+
+const goToCityView = (city) => {
+  router.push({
+    name: "cityView",
+    params: {
+      state: city.state,
+      city: city.city,
+    },
+    query: {
+      lat: city.coords.lat,
+      lng: city.coords.lng,
+    },
+  });
+};
+</script>
